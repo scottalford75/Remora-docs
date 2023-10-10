2c587904bdc31aaf787ab34cc62616ff1da94984
scott <scottalford75@gmail.com>
2023-10-11 07:47:52 +1100

    updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Remora'
+copyright = '2023, Scott Alford aka scotta'
+author = 'Scott Alford'
 
 release = '0.1'
 version = '0.1.0'
